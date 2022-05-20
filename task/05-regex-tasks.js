d3648a12ba5c2df52d0abf1836729cfe58654faa
Pavel Shytsikau <pavel.shytsikau@pstlabs.by>
2022-05-20 09:50:04 +0300

    no message

--- task/05-regex-tasks.js
+++ task/05-regex-tasks.js
@@ -72,7 +72,7 @@ function getRegexForPitSpot() {
  * @return {RegExp}
  */
 function getRegexForIPv4() {
-   throw new Error('Not implemented');
+   return \b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b;
 }
 
 

613797bdb789045a641bddf5ab45fe9cfd742405
Pavel Shytsikau <pavel.shytsikau@pstlabs.by>
2022-05-26 09:05:18 +0300

    no message

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -129,7 +129,12 @@ function isTriangle(a,b,c) {
  *  
  */
 function doRectanglesOverlap(rect1, rect2) {
-    throw new Error('Not implemented');
+    return !(
+        (rect2.left < rect1.left && (rect2.left + rect2.width) < rect1.left) ||
+        (rect2.top < rect1.top && (rect2.top + rect2.height) < rect1.top) ||
+        (rect1.left < rect2.left && (rect1.left + rect1.width) < rect2.left) ||
+        (rect1.top < rect2.top && (rect1.top + rect1.height) < rect2.top)
+    );
 }
 
 
